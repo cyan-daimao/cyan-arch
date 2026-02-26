1b405ffcfe20c54e663b8fc8840d96c81024e807
cy.Y <chenyang.yan@qlchat.com>
2026-02-26 17:48:34 +0800

    增加登陆拦截器

--- arch-base/src/main/java/com/cyan/arch/base/apater/http/config/WebConfig.java
+++ arch-base/src/main/java/com/cyan/arch/base/apater/http/config/WebConfig.java
@@ -40,7 +40,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns(
                         "/health",
                         "/login",
-                        "/rpc/**"
+                        "/rpc/**",
+                        "public/**"
                 );
     }
 }

2146dee7940e585bf1ef5a318afcd356f226a053
cy.Y <daimao2817@gmail.com>
2026-03-02 00:51:48 +0800

    调整登录模块

--- arch-base/src/main/java/com/cyan/arch/base/apater/http/config/WebConfig.java
+++ arch-base/src/main/java/com/cyan/arch/base/apater/http/config/WebConfig.java
@@ -1,11 +1,8 @@
 package com.cyan.arch.base.apater.http.config;
 
-import com.cyan.arch.base.apater.http.filter.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,13 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    // 关键3：注入 Spring 管理的 LoginInterceptor Bean（而非手动 new）
-    private final LoginInterceptor loginInterceptor;
-
-    // 构造器注入拦截器
-    public WebConfig(@Lazy LoginInterceptor loginInterceptor) {
-        this.loginInterceptor = loginInterceptor;
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -33,15 +23,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600); // 预检请求缓存时间（秒）
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/health",
-                        "/login",
-                        "/rpc/**",
-                        "/public/**"
-                );
-    }
 }
